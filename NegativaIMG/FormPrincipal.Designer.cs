845a9661fdca5521667d6d93a8951cfd665fab2c
Livisssss <lverlindo@yahoo.com.br>
2024-03-18 23:04:06 -0300

    Add project files.

---
+++ NegativaIMG/FormPrincipal.Designer.cs
@@ -0,0 +1,104 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace NegativaIMG
+{
+    partial class FormPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormPrincipal));
+            this.btCarregar = new System.Windows.Forms.Button();
+            this.btDownload = new System.Windows.Forms.Button();
+            this.pictureBoxOriginal = new System.Windows.Forms.PictureBox();
+            this.pictureBoxNegativa = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxOriginal)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxNegativa)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btCarregar
+            // 
+            this.btCarregar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btCarregar.Font = new System.Drawing.Font("Arial", 12F);
+            this.btCarregar.Location = new System.Drawing.Point(50, 20);
+            this.btCarregar.Name = "btCarregar";
+            this.btCarregar.Size = new System.Drawing.Size(400, 34);
+            this.btCarregar.TabIndex = 0;
+            this.btCarregar.Text = "Carregar Imagem";
+            this.btCarregar.Click += new System.EventHandler(this.btCarregar_Click);
+            // 
+            // btDownload
+            // 
+            this.btDownload.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btDownload.Enabled = false;
+            this.btDownload.Font = new System.Drawing.Font("Arial", 12F);
+            this.btDownload.Location = new System.Drawing.Point(538, 20);
+            this.btDownload.Name = "btDownload";
+            this.btDownload.Size = new System.Drawing.Size(400, 34);
+            this.btDownload.TabIndex = 1;
+            this.btDownload.Text = "Download Negativo";
+            this.btDownload.Click += new System.EventHandler(this.btDownload_Click);
+            // 
+            // pictureBoxOriginal
+            // 
+            this.pictureBoxOriginal.Location = new System.Drawing.Point(50, 60);
+            this.pictureBoxOriginal.Name = "pictureBoxOriginal";
+            this.pictureBoxOriginal.Size = new System.Drawing.Size(400, 400);
+            this.pictureBoxOriginal.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxOriginal.TabIndex = 2;
+            this.pictureBoxOriginal.TabStop = false;
+            // 
+            // pictureBoxNegativa
+            // 
+            this.pictureBoxNegativa.Location = new System.Drawing.Point(538, 60);
+            this.pictureBoxNegativa.Name = "pictureBoxNegativa";
+            this.pictureBoxNegativa.Size = new System.Drawing.Size(400, 400);
+            this.pictureBoxNegativa.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxNegativa.TabIndex = 3;
+            this.pictureBoxNegativa.TabStop = false;
+            // 
+            // FormPrincipal
+            // 
+            this.ClientSize = new System.Drawing.Size(1035, 505);
+            this.Controls.Add(this.btCarregar);
+            this.Controls.Add(this.btDownload);
+            this.Controls.Add(this.pictureBoxOriginal);
+            this.Controls.Add(this.pictureBoxNegativa);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FormPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Aplicação de Negativo em Imagem";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxOriginal)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxNegativa)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ NegativaIMG/FormPrincipal.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Windows.Forms;
+
+namespace NegativaIMG
+{
+    public partial class FormPrincipal : Form
+    {
+        private PictureBox pictureBoxOriginal;
+        private PictureBox pictureBoxNegativa;
+        private Button btCarregar;
+        private Button btDownload;
+        private Bitmap imagemOriginal;
+        private Bitmap imagemNegativa;
+
+        public FormPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void btCarregar_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Imagens|*.jpg;*.jpeg;*.png;*.bmp;*.gif";
+            openFileDialog.Title = "Selecione a imagem";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string imagemPath = openFileDialog.FileName;
+
+                // Carrega a imagem original
+                imagemOriginal = new Bitmap(imagemPath);
+                pictureBoxOriginal.Image = imagemOriginal;
+
+                // Calcula o negativo da imagem
+                imagemNegativa = new Bitmap(imagemOriginal.Width, imagemOriginal.Height);
+                for (int y = 0; y < imagemOriginal.Height; y++)
+                {
+                    for (int x = 0; x < imagemOriginal.Width; x++)
+                    {
+                        Color pixelOriginal = imagemOriginal.GetPixel(x, y);
+                        Color pixelNegativo = Color.FromArgb(255 - pixelOriginal.R, 255 - pixelOriginal.G, 255 - pixelOriginal.B);
+                        imagemNegativa.SetPixel(x, y, pixelNegativo);
+                    }
+                }
+                pictureBoxNegativa.Image = imagemNegativa;
+                btDownload.Enabled = true; // Habilita o botão de download
+            }
+        }
+
+        private void btDownload_Click(object sender, EventArgs e)
+        {
+            if (imagemNegativa != null)
+            {
+                SaveFileDialog saveFileDialog = new SaveFileDialog();
+                saveFileDialog.Filter = "Imagens|*.jpg;*.jpeg;*.png;*.bmp;*.gif";
+                saveFileDialog.Title = "Salvar Imagem Negativa";
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    imagemNegativa.Save(saveFileDialog.FileName, ImageFormat.Jpeg);
+                }
+            }
+        }
+    }
+}
