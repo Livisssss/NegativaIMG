4b66fabe5ab9c225af428198b01ad226d637df8b
Livisssss <lverlindo@yahoo.com.br>
2024-03-26 00:43:07 -0300

    Design

--- NegativaIMG/FormPrincipal.Designer.cs
+++ NegativaIMG/FormPrincipal.Designer.cs
@@ -32,73 +32,316 @@ namespace NegativaIMG
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormPrincipal));
-            this.btCarregar = new System.Windows.Forms.Button();
+            this.btCarregaImgA = new System.Windows.Forms.Button();
             this.btDownload = new System.Windows.Forms.Button();
             this.pictureBoxOriginal = new System.Windows.Forms.PictureBox();
-            this.pictureBoxNegativa = new System.Windows.Forms.PictureBox();
+            this.pictureBoxResultado = new System.Windows.Forms.PictureBox();
+            this.btNegativo = new System.Windows.Forms.Button();
+            this.pictureBoxSegunda = new System.Windows.Forms.PictureBox();
+            this.btCarregaImgB = new System.Windows.Forms.Button();
+            this.gbA = new System.Windows.Forms.GroupBox();
+            this.gbB = new System.Windows.Forms.GroupBox();
+            this.gbR = new System.Windows.Forms.GroupBox();
+            this.btSomar = new System.Windows.Forms.Button();
+            this.gbSelecao = new System.Windows.Forms.GroupBox();
+            this.rbA = new System.Windows.Forms.RadioButton();
+            this.rbB = new System.Windows.Forms.RadioButton();
+            this.rbAB = new System.Windows.Forms.RadioButton();
+            this.gbOpArit = new System.Windows.Forms.GroupBox();
+            this.btSubtrair = new System.Windows.Forms.Button();
+            this.gbGirar = new System.Windows.Forms.GroupBox();
+            this.btFlipLR = new System.Windows.Forms.Button();
+            this.btFlipUD = new System.Windows.Forms.Button();
+            this.gbRecortar = new System.Windows.Forms.GroupBox();
+            this.gbConcatenar = new System.Windows.Forms.GroupBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxOriginal)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxNegativa)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxResultado)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxSegunda)).BeginInit();
+            this.gbA.SuspendLayout();
+            this.gbB.SuspendLayout();
+            this.gbR.SuspendLayout();
+            this.gbSelecao.SuspendLayout();
+            this.gbOpArit.SuspendLayout();
+            this.gbGirar.SuspendLayout();
             this.SuspendLayout();
             // 
-            // btCarregar
+            // btCarregaImgA
             // 
-            this.btCarregar.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btCarregar.Font = new System.Drawing.Font("Arial", 12F);
-            this.btCarregar.Location = new System.Drawing.Point(50, 20);
-            this.btCarregar.Name = "btCarregar";
-            this.btCarregar.Size = new System.Drawing.Size(400, 34);
-            this.btCarregar.TabIndex = 0;
-            this.btCarregar.Text = "Carregar Imagem";
-            this.btCarregar.Click += new System.EventHandler(this.btCarregar_Click);
+            this.btCarregaImgA.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btCarregaImgA.Font = new System.Drawing.Font("Arial", 12F);
+            this.btCarregaImgA.Location = new System.Drawing.Point(16, 29);
+            this.btCarregaImgA.Name = "btCarregaImgA";
+            this.btCarregaImgA.Size = new System.Drawing.Size(280, 34);
+            this.btCarregaImgA.TabIndex = 0;
+            this.btCarregaImgA.Text = "Carregar";
+            this.btCarregaImgA.Click += new System.EventHandler(this.btCarregaImgA_Click);
             // 
             // btDownload
             // 
             this.btDownload.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btDownload.Enabled = false;
             this.btDownload.Font = new System.Drawing.Font("Arial", 12F);
-            this.btDownload.Location = new System.Drawing.Point(538, 20);
+            this.btDownload.Location = new System.Drawing.Point(6, 19);
             this.btDownload.Name = "btDownload";
-            this.btDownload.Size = new System.Drawing.Size(400, 34);
+            this.btDownload.Size = new System.Drawing.Size(280, 34);
             this.btDownload.TabIndex = 1;
-            this.btDownload.Text = "Download Negativo";
+            this.btDownload.Text = "Download Imagem";
             this.btDownload.Click += new System.EventHandler(this.btDownload_Click);
             // 
             // pictureBoxOriginal
             // 
-            this.pictureBoxOriginal.Location = new System.Drawing.Point(50, 60);
+            this.pictureBoxOriginal.AccessibleName = "";
+            this.pictureBoxOriginal.Location = new System.Drawing.Point(16, 69);
             this.pictureBoxOriginal.Name = "pictureBoxOriginal";
-            this.pictureBoxOriginal.Size = new System.Drawing.Size(400, 400);
+            this.pictureBoxOriginal.Size = new System.Drawing.Size(280, 280);
             this.pictureBoxOriginal.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBoxOriginal.TabIndex = 2;
             this.pictureBoxOriginal.TabStop = false;
+            this.pictureBoxOriginal.Tag = "Imagem 1";
             // 
-            // pictureBoxNegativa
+            // pictureBoxResultado
             // 
-            this.pictureBoxNegativa.Location = new System.Drawing.Point(538, 60);
-            this.pictureBoxNegativa.Name = "pictureBoxNegativa";
-            this.pictureBoxNegativa.Size = new System.Drawing.Size(400, 400);
-            this.pictureBoxNegativa.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBoxNegativa.TabIndex = 3;
-            this.pictureBoxNegativa.TabStop = false;
+            this.pictureBoxResultado.Location = new System.Drawing.Point(6, 59);
+            this.pictureBoxResultado.Name = "pictureBoxResultado";
+            this.pictureBoxResultado.Size = new System.Drawing.Size(280, 280);
+            this.pictureBoxResultado.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxResultado.TabIndex = 3;
+            this.pictureBoxResultado.TabStop = false;
+            // 
+            // btNegativo
+            // 
+            this.btNegativo.Location = new System.Drawing.Point(525, 548);
+            this.btNegativo.Name = "btNegativo";
+            this.btNegativo.Size = new System.Drawing.Size(93, 27);
+            this.btNegativo.TabIndex = 4;
+            this.btNegativo.Text = "Aplicar Negativo";
+            this.btNegativo.UseVisualStyleBackColor = true;
+            this.btNegativo.Click += new System.EventHandler(this.btNegativo_Click);
+            // 
+            // pictureBoxSegunda
+            // 
+            this.pictureBoxSegunda.AccessibleName = "";
+            this.pictureBoxSegunda.Location = new System.Drawing.Point(16, 69);
+            this.pictureBoxSegunda.Name = "pictureBoxSegunda";
+            this.pictureBoxSegunda.Size = new System.Drawing.Size(280, 280);
+            this.pictureBoxSegunda.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxSegunda.TabIndex = 5;
+            this.pictureBoxSegunda.TabStop = false;
+            // 
+            // btCarregaImgB
+            // 
+            this.btCarregaImgB.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btCarregaImgB.Font = new System.Drawing.Font("Arial", 12F);
+            this.btCarregaImgB.Location = new System.Drawing.Point(16, 29);
+            this.btCarregaImgB.Name = "btCarregaImgB";
+            this.btCarregaImgB.Size = new System.Drawing.Size(280, 34);
+            this.btCarregaImgB.TabIndex = 6;
+            this.btCarregaImgB.Text = "Carregar";
+            this.btCarregaImgB.Click += new System.EventHandler(this.btCarregaImgB_Click);
+            // 
+            // gbA
+            // 
+            this.gbA.Controls.Add(this.btCarregaImgA);
+            this.gbA.Controls.Add(this.pictureBoxOriginal);
+            this.gbA.Location = new System.Drawing.Point(21, 20);
+            this.gbA.Name = "gbA";
+            this.gbA.Size = new System.Drawing.Size(311, 355);
+            this.gbA.TabIndex = 7;
+            this.gbA.TabStop = false;
+            this.gbA.Text = "Imagem A";
+            // 
+            // gbB
+            // 
+            this.gbB.Controls.Add(this.btCarregaImgB);
+            this.gbB.Controls.Add(this.pictureBoxSegunda);
+            this.gbB.Location = new System.Drawing.Point(21, 411);
+            this.gbB.Name = "gbB";
+            this.gbB.Size = new System.Drawing.Size(311, 355);
+            this.gbB.TabIndex = 8;
+            this.gbB.TabStop = false;
+            this.gbB.Text = "Imagem B";
+            // 
+            // gbR
+            // 
+            this.gbR.Controls.Add(this.btDownload);
+            this.gbR.Controls.Add(this.pictureBoxResultado);
+            this.gbR.Location = new System.Drawing.Point(919, 12);
+            this.gbR.Name = "gbR";
+            this.gbR.Size = new System.Drawing.Size(293, 342);
+            this.gbR.TabIndex = 9;
+            this.gbR.TabStop = false;
+            this.gbR.Text = "Resultado";
+            // 
+            // btSomar
+            // 
+            this.btSomar.Location = new System.Drawing.Point(6, 19);
+            this.btSomar.Name = "btSomar";
+            this.btSomar.Size = new System.Drawing.Size(102, 27);
+            this.btSomar.TabIndex = 10;
+            this.btSomar.Text = "Somar Imagens";
+            this.btSomar.UseVisualStyleBackColor = true;
+            // 
+            // gbSelecao
+            // 
+            this.gbSelecao.Controls.Add(this.rbAB);
+            this.gbSelecao.Controls.Add(this.rbB);
+            this.gbSelecao.Controls.Add(this.rbA);
+            this.gbSelecao.Location = new System.Drawing.Point(367, 71);
+            this.gbSelecao.Name = "gbSelecao";
+            this.gbSelecao.Size = new System.Drawing.Size(106, 100);
+            this.gbSelecao.TabIndex = 11;
+            this.gbSelecao.TabStop = false;
+            this.gbSelecao.Text = "1. Seleção";
+            // 
+            // rbA
+            // 
+            this.rbA.AutoSize = true;
+            this.rbA.Location = new System.Drawing.Point(7, 20);
+            this.rbA.Name = "rbA";
+            this.rbA.Size = new System.Drawing.Size(72, 17);
+            this.rbA.TabIndex = 0;
+            this.rbA.TabStop = true;
+            this.rbA.Text = "Imagem A";
+            this.rbA.UseVisualStyleBackColor = true;
+            // 
+            // rbB
+            // 
+            this.rbB.AutoSize = true;
+            this.rbB.Location = new System.Drawing.Point(7, 44);
+            this.rbB.Name = "rbB";
+            this.rbB.Size = new System.Drawing.Size(72, 17);
+            this.rbB.TabIndex = 1;
+            this.rbB.TabStop = true;
+            this.rbB.Text = "Imagem B";
+            this.rbB.UseVisualStyleBackColor = true;
+            // 
+            // rbAB
+            // 
+            this.rbAB.AutoSize = true;
+            this.rbAB.Location = new System.Drawing.Point(7, 68);
+            this.rbAB.Name = "rbAB";
+            this.rbAB.Size = new System.Drawing.Size(57, 17);
+            this.rbAB.TabIndex = 2;
+            this.rbAB.TabStop = true;
+            this.rbAB.Text = "Ambas";
+            this.rbAB.UseVisualStyleBackColor = true;
+            // 
+            // gbOpArit
+            // 
+            this.gbOpArit.Controls.Add(this.btSubtrair);
+            this.gbOpArit.Controls.Add(this.btSomar);
+            this.gbOpArit.Location = new System.Drawing.Point(525, 71);
+            this.gbOpArit.Name = "gbOpArit";
+            this.gbOpArit.Size = new System.Drawing.Size(151, 100);
+            this.gbOpArit.TabIndex = 12;
+            this.gbOpArit.TabStop = false;
+            this.gbOpArit.Text = "Operações Aritméticas:";
+            // 
+            // btSubtrair
+            // 
+            this.btSubtrair.Location = new System.Drawing.Point(6, 52);
+            this.btSubtrair.Name = "btSubtrair";
+            this.btSubtrair.Size = new System.Drawing.Size(102, 27);
+            this.btSubtrair.TabIndex = 11;
+            this.btSubtrair.Text = "Subtrair Imagens";
+            this.btSubtrair.UseVisualStyleBackColor = true;
+            // 
+            // gbGirar
+            // 
+            this.gbGirar.Controls.Add(this.btFlipLR);
+            this.gbGirar.Controls.Add(this.btFlipUD);
+            this.gbGirar.Location = new System.Drawing.Point(525, 187);
+            this.gbGirar.Name = "gbGirar";
+            this.gbGirar.Size = new System.Drawing.Size(151, 100);
+            this.gbGirar.TabIndex = 13;
+            this.gbGirar.TabStop = false;
+            this.gbGirar.Text = "Girar:";
+            // 
+            // btFlipLR
+            // 
+            this.btFlipLR.Location = new System.Drawing.Point(6, 55);
+            this.btFlipLR.Name = "btFlipLR";
+            this.btFlipLR.Size = new System.Drawing.Size(102, 27);
+            this.btFlipLR.TabIndex = 13;
+            this.btFlipLR.Text = "Flip Left/Rigth";
+            this.btFlipLR.UseVisualStyleBackColor = true;
+            // 
+            // btFlipUD
+            // 
+            this.btFlipUD.Location = new System.Drawing.Point(6, 22);
+            this.btFlipUD.Name = "btFlipUD";
+            this.btFlipUD.Size = new System.Drawing.Size(102, 27);
+            this.btFlipUD.TabIndex = 12;
+            this.btFlipUD.Text = "Flip Up/Down";
+            this.btFlipUD.UseVisualStyleBackColor = true;
+            // 
+            // gbRecortar
+            // 
+            this.gbRecortar.Location = new System.Drawing.Point(525, 308);
+            this.gbRecortar.Name = "gbRecortar";
+            this.gbRecortar.Size = new System.Drawing.Size(151, 100);
+            this.gbRecortar.TabIndex = 14;
+            this.gbRecortar.TabStop = false;
+            this.gbRecortar.Text = "Recortar:";
+            // 
+            // gbConcatenar
+            // 
+            this.gbConcatenar.Location = new System.Drawing.Point(525, 423);
+            this.gbConcatenar.Name = "gbConcatenar";
+            this.gbConcatenar.Size = new System.Drawing.Size(151, 100);
+            this.gbConcatenar.TabIndex = 15;
+            this.gbConcatenar.TabStop = false;
+            this.gbConcatenar.Text = "Concatenar:";
             // 
             // FormPrincipal
             // 
-            this.ClientSize = new System.Drawing.Size(1035, 505);
-            this.Controls.Add(this.btCarregar);
-            this.Controls.Add(this.btDownload);
-            this.Controls.Add(this.pictureBoxOriginal);
-            this.Controls.Add(this.pictureBoxNegativa);
+            this.ClientSize = new System.Drawing.Size(1224, 855);
+            this.Controls.Add(this.gbConcatenar);
+            this.Controls.Add(this.gbRecortar);
+            this.Controls.Add(this.gbGirar);
+            this.Controls.Add(this.gbOpArit);
+            this.Controls.Add(this.btNegativo);
+            this.Controls.Add(this.gbSelecao);
+            this.Controls.Add(this.gbR);
+            this.Controls.Add(this.gbB);
+            this.Controls.Add(this.gbA);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "FormPrincipal";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Aplicação de Negativo em Imagem";
+            this.Text = "Edições em Imagem";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxOriginal)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxNegativa)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxResultado)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxSegunda)).EndInit();
+            this.gbA.ResumeLayout(false);
+            this.gbB.ResumeLayout(false);
+            this.gbR.ResumeLayout(false);
+            this.gbSelecao.ResumeLayout(false);
+            this.gbSelecao.PerformLayout();
+            this.gbOpArit.ResumeLayout(false);
+            this.gbGirar.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private Button btNegativo;
+        private PictureBox pictureBoxSegunda;
+        private Button btCarregaImgB;
+        private GroupBox gbA;
+        private GroupBox gbB;
+        private GroupBox gbR;
+        private Button btSomar;
+        private GroupBox gbSelecao;
+        private RadioButton rbAB;
+        private RadioButton rbB;
+        private RadioButton rbA;
+        private GroupBox gbOpArit;
+        private Button btSubtrair;
+        private GroupBox gbGirar;
+        private Button btFlipLR;
+        private Button btFlipUD;
+        private GroupBox gbRecortar;
+        private GroupBox gbConcatenar;
     }
 }
-
--- NegativaIMG/FormPrincipal.cs
+++ NegativaIMG/FormPrincipal.cs
@@ -9,44 +9,94 @@ namespace NegativaIMG
     public partial class FormPrincipal : Form
     {
         private PictureBox pictureBoxOriginal;
-        private PictureBox pictureBoxNegativa;
-        private Button btCarregar;
+        private PictureBox pictureBoxResultado;
+        private Button btCarregaImgA;
         private Button btDownload;
-        private Bitmap imagemOriginal;
+        private Bitmap imgA;
         private Bitmap imagemNegativa;
+        private int[] pixelData; // Array para armazenar os valores ARGB dos pixels
 
         public FormPrincipal()
         {
             InitializeComponent();
         }
-
-        private void btCarregar_Click(object sender, EventArgs e)
+        private void btCarregaImgA_Click(object sender, EventArgs e)
         {
             OpenFileDialog openFileDialog = new OpenFileDialog();
-            openFileDialog.Filter = "Imagens|*.jpg;*.jpeg;*.png;*.bmp;*.gif";
-            openFileDialog.Title = "Selecione a imagem";
+            openFileDialog.Filter = "Arquivos de imagem (**.jpg, *.jpeg, *.png *.tif)|*.jpg;*.jpeg;*.png; *.tif";
 
-            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            DialogResult result = openFileDialog.ShowDialog();
+
+            if (result == DialogResult.OK)
             {
-                string imagemPath = openFileDialog.FileName;
+                imgA = new Bitmap(openFileDialog.FileName);
+                pictureBoxOriginal.Image = imgA;
 
-                // Carrega a imagem original
-                imagemOriginal = new Bitmap(imagemPath);
-                pictureBoxOriginal.Image = imagemOriginal;
+                SalvarImagemEmArray(imgA);
 
-                // Calcula o negativo da imagem
-                imagemNegativa = new Bitmap(imagemOriginal.Width, imagemOriginal.Height);
-                for (int y = 0; y < imagemOriginal.Height; y++)
+            }
+            else
+            {
+                MessageBox.Show("Selecione uma imagem válida");
+            }
+        }
+
+        private void btCarregaImgB_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Arquivos de imagem (*.jpg, *.jpeg, *.png *.tif)|*.jpg;*.jpeg;*.png; *.tif";
+
+            DialogResult result = openFileDialog.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                // Carrega a segunda imagem
+                Bitmap imgB = new Bitmap(openFileDialog.FileName);
+
+                imgB = new Bitmap(openFileDialog.FileName);
+                pictureBoxSegunda.Image = imgB;
+
+                // Salva a segunda imagem em um array
+                SalvarImagemEmArray(imgB);
+            }
+            else
+            {
+                MessageBox.Show("Selecione uma imagem válida");
+            }
+        }
+
+
+        private void SalvarImagemEmArray(Bitmap imagem)
+        {
+            int largura = imagem.Width;
+            int altura = imagem.Height;
+
+            // Cria um array para armazenar os valores ARGB dos pixels da imagem
+            pixelData = new int[largura * altura];
+
+            // Preenche o array com os valores ARGB dos pixels da imagem
+            int index = 0;
+            for (int y = 0; y < altura; y++)
+            {
+                for (int x = 0; x < largura; x++)
                 {
-                    for (int x = 0; x < imagemOriginal.Width; x++)
-                    {
-                        Color pixelOriginal = imagemOriginal.GetPixel(x, y);
-                        Color pixelNegativo = Color.FromArgb(255 - pixelOriginal.R, 255 - pixelOriginal.G, 255 - pixelOriginal.B);
-                        imagemNegativa.SetPixel(x, y, pixelNegativo);
-                    }
+                    Color pixel = imagem.GetPixel(x, y);
+                    pixelData[index] = pixel.ToArgb();
+                    index++;
                 }
-                pictureBoxNegativa.Image = imagemNegativa;
-                btDownload.Enabled = true; // Habilita o botão de download
+            }
+        }
+
+        private void btNegativo_Click(object sender, EventArgs e)
+        {
+            if (imgA != null)
+            {
+                // Calcula o negativo da imagem original
+                CalcularNegativo();
+            }
+            else
+            {
+                MessageBox.Show("O negativo não pode ser aplicado, pois não há imagem carregada.", "Falha", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
         }
 
@@ -56,12 +106,60 @@ namespace NegativaIMG
             {
                 SaveFileDialog saveFileDialog = new SaveFileDialog();
                 saveFileDialog.Filter = "Imagens|*.jpg;*.jpeg;*.png;*.bmp;*.gif";
-                saveFileDialog.Title = "Salvar Imagem Negativa";
+                saveFileDialog.Title = "Salvar Imagem";
                 if (saveFileDialog.ShowDialog() == DialogResult.OK)
                 {
                     imagemNegativa.Save(saveFileDialog.FileName, ImageFormat.Jpeg);
                 }
             }
+            else
+            {
+                MessageBox.Show("Não há imagem para salvar.", "Falha", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
         }
+
+
+
+
+
+
+
+        // funções
+        private void CalcularNegativo()
+        {
+            if (pixelData != null)
+            {
+                int largura = imgA.Width;
+                int altura = imgA.Height;
+
+                // Inicializa a imagem negativa com as mesmas dimensões que a imagem original
+                imagemNegativa = new Bitmap(largura, altura);
+
+                // Itera sobre cada pixel no array de dados e aplica o negativo
+                for (int i = 0; i < pixelData.Length; i++)
+                {
+                    // Obtém os componentes ARGB do pixel
+                    int argb = pixelData[i];
+
+                    int alpha = (argb >> 24) & 0xFF;  // >> para deslocar os bits para a direita e & 0xFF para aplicar a máscara e pegar apenas os 8 bits menos significativos
+                    int red = (argb >> 16) & 0xFF;
+                    int green = (argb >> 8) & 0xFF;
+                    int blue = argb & 0xFF;
+
+                    // Aplica o negativo, formando novo valor ARGB para representar a cor do pixel negativo fazendo -255
+                    int negativo = (alpha << 24) | ((255 - red) << 16) | ((255 - green) << 8) | (255 - blue);
+
+                    // Define o pixel negativo na imagem negativa   % nos da a coordenada x (posição horizontal) e / nos da a coordenada y (posição vertical)
+                    imagemNegativa.SetPixel(i % largura, i / largura, Color.FromArgb(negativo));
+                }
+
+                // Exibe a imagem negativa no pictureBoxNegativa
+                pictureBoxResultado.Image = imagemNegativa;
+
+                // Habilita o botão de download
+                btDownload.Enabled = true;
+            }
+        }
+
     }
 }
